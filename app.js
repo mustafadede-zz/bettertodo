cc9e4c16177b029b4fe7e2a2c74dc522d829e3d6
PC Principal <ozanbaskan7@gmail.com>
2021-08-06 11:24:08 +0300

    function fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let taskname = ""
-let taskdesc = ""
+let taskname = "";
+let taskdesc = "";
 
 // query selector kullanıldığında geriye dönen değer o sayfada verilen class veya tag e uygun düşen elementlerden oluşan bir arraydir.
 // sayfaya bu tanıma uygun başka elementler koyulduğunda bu tanımlamaların kodun bozulmasına yol açar.
@@ -8,46 +8,41 @@ let taskdesc = ""
 // bir elementin bütün child elementlerini döndüren recursive mantıkla çalışan bir fonksiyon yazdım.
 // anlaması şimdilik zor gelebilir. O yüzden açıklamadım, bu konuya çalışırken geri dönüp pratik için bakılabilir.
 const getAllChildren = (htmlElement) => {
+  if (htmlElement.children.length === 0) return [htmlElement];
 
-    if (htmlElement.children.length === 0) return [htmlElement];
+  let allChildElements = [];
 
-    allChildElements = [];
+  for (let i = 0; i < htmlElement.children.length; i++) {
+    let children = getAllChildren(htmlElement.children[i]);
+    if (children) allChildElements.push(...children);
+  }
+  allChildElements.push(htmlElement);
 
-    for (let i = 0; i <htmlElement.children.length; i++)
-    {
-        let children = getAllChildren(htmlElement.children[i]);
-        if (children) allChildElements = [htmlElement, ...children];
-    }
-    
-    return allChildElements;
-}
+  return allChildElements;
+};
 
-
-
-const button = document.body.querySelector(".button")
+const button = document.body.querySelector(".button");
 const buttonContents = getAllChildren(button);
 
-const modal = document.body.querySelector(".modal")
+const modal = document.body.querySelector(".modal");
 const modalContents = getAllChildren(modal);
 
-const cardelement = `<div class='box'><div class='card-items'><p class='card-title'>${taskname}</p><hr><p class='card-desc'>${taskdesc}</p></div></div>`
+const cardelement = `<div class='box'><div class='card-items'><p class='card-title'>${taskname}</p><hr><p class='card-desc'>${taskdesc}</p></div></div>`;
 
 function showDO() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // butona eventListener ekledim.
 button.addEventListener("click", showDO);
-
 console.log(buttonContents);
-console.log(modalContents);
-document.addEventListener("click",function (event) {
 
-    // targeti birden fazla kullanacağim için değişkene atadım.
-    // böylelikle kodun okunabilirliği kolaylaştı.
-    const target = event.target;
+document.addEventListener("click", function (event) {
+  // targeti birden fazla kullanacağim için değişkene atadım.
+  // böylelikle kodun okunabilirliği kolaylaştı.
+  const target = event.target;
 
-    // logic kısmı burada, if statement kullanırken tek satır kod yazıyorsak küme parantezi kullanmamıza gerek yok.
-    if (![...buttonContents, ...modalContents].includes(target)) 
-        modal.style.display = "none";
-})
+  // logic kısmı burada, if statement kullanırken tek satır kod yazıyorsak küme parantezi kullanmamıza gerek yok.
+  if (![...buttonContents, ...modalContents].includes(target))
+    modal.style.display = "none";
+});
